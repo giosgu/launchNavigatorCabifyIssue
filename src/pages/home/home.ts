ca41c96a7d659d35b368233d737c07325f75b92d
GIOSA GUIDO <MT30886835@osdebinario.grupoosde>
2019-10-10 21:41:16 -0300

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LaunchNavigator } from '@ionic-native/launch-navigator';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,  public launchNavigator:LaunchNavigator) {
 
   }
 
+  comoLlegar(){
+    this.launchNavigator.navigate("-34.545073, -58.449725", {
+      appSelection: {
+        dialogHeaderText: "Seleccione la app asociada",
+        cancelButtonText: "Cancelar",
+      }
+    })
+    .then(
+      success => console.log('Launched navigator'),
+      error => alert('Hubo un error al comenzar la navegación ' +  error)
+    );
+
+  }
 }
